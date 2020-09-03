8999d70d8c7128abc58b63e9fbd000942711b574
Sanna <santras@users.noreply.github.com>
2020-09-03 19:50:10 +0300

    siirretty reunatapaukset testaa funktioon

--- queenknight.py
+++ queenknight.py
@@ -3,67 +3,62 @@
 
 from array import array
 
-def heppahyokkaus(heppa,nn,hepan_s0ind):
-    # Tämä antaa listana indeksit hepan ruuduille, kun nn on ruudukon sivun pituus.
-    # Lisäksi tämä antaa boolean listan ovatko liikket mahdollisia.
-    # yla1,yla2,vas1,vas2, ala1,ala2,oik1,oik2
-   
-    #print(heppa,nn,hepan_s0ind)
+#def kuningatar(heppa,nn,sopimattomat):
 
-    yla1 = (heppa-2*nn-1)
-    yla2 = (heppa-2*nn+1)
-    vas1 = (heppa-nn-2)
-    vas2 = (heppa+nn+-2)
-    ala1 = (heppa+2*nn-1)
-    ala2 = (heppa+2*nn+1)
-    oik1 = (heppa-nn+2)
-    oik2 = (heppa+nn+2)
+def heppahyokkaus(heppa,nn):
+    #Tämä antaa listana indeksit hepan mahdollisille hyökkäysruuduille, kun nn on ruudukon sivun pituus.
+    #yla1,yla2,vas1,vas2, ala1,ala2,oik1,oik2
+
+    yla1 = (heppa-2*nn-1) # 0
+    yla2 = (heppa-2*nn+1) #1
+    vas1 = (heppa-nn-2)   #2 =vas ylä
+    vas2 = (heppa+nn+-2)  #3 =vas ala
+    ala1 = (heppa+2*nn-1) #4
+    ala2 = (heppa+2*nn+1) #5
+    oik1 = (heppa-nn+2)   #6 =oik ylä
+    oik2 = (heppa+nn+2)   #7  =oik ala
     
-    liikkeet = [yla1,yla2,vas1,vas2,ala1,ala2,oik1,oik2]
-    onko_laudalla = [True]*8
-    print(liikkeet)
+    return [yla1,yla2,vas1,vas2,ala1,ala2,oik1,oik2]
     
-    for ii in range(len(liikkeet)):
-        #print(ii)
-        if liikkeet[ii]<0:                             # ylilaudan
-            #print('heps',ii)
-            onko_laudalla[ii] = False  
-        elif liikkeet[ii]>(nn*nn-1):                    # alilaudan
-            onko_laudalla[ii] = False
-        if ii in [2,3,6,7]:
-            if (liikkeet[2]<hepan_s0ind-nn): # vasen sivu, ylä
-                print(ii,liikkeet[2],liikkeet[ii],hepan_s0ind-nn)
-                onko_laudalla[2] = False
-            elif (liikkeet[3]<hepan_s0ind+nn): # vasen sivu, ala
-                onko_laudalla[3] = False
-            elif (liikkeet[6]>hepan_s0ind-1): # oikea sivu, ylä
-                onko_laudalla[6] = False
-            elif liikkeet[7]>hepan_s0ind+nn-1: # oikea sivu, ala
-                onko_laudalla[7] = False
-   #print(liikkeet, onko_laudalla)
-
-# def hepparuudut(paikka,s0_ind,nn):
-#     if paikka >= s0_ind[2] : # eli ei kahdella ekalla rivillä  # TARKISTA INDEKSIT!
-#         if paikka <=s0_ind[nn-3]: # ei kahdella vikalla rivillä
-#             #JOTAIN
-#         elif paikka <=s0_ind[nn-2]: # tokavikarivi
-#             #JOTAIN     
-#         else:                       # vikarivi
-#             #JOTAIN
-#     elif
-
 
 
 def testaa(heppa,poyta,s0_ind,nn):
 
     laskin=0
-    # määrittele sopimattomat paikat kun heppa paikassa heppa eli heppa ja mahdolliset ruudut x0-24
 
-    #testaa loput paikoista sopiiko vai ei + laske sopivien määrä
+    #erikoistapaukset reunoilla
+    ignoorauslista = [False]*8
+    if heppa<s0_ind[0]:             #yläreunan liikkeet ei mahdollisia
+        ignoorauslista[0] = True    #reuna 1 ruudun päässä -> poista ylä
+        ignoorauslista[1] = True
+    elif heppa<s0_ind[1]:
+        ignoorauslista[0] = True    #heppa reunalla ->poista ylä + sivu ylät
+        ignoorauslista[1] = True    
+        ignoorauslista[2] = True    
+        ignoorauslista[6] = True
+    elif heppa>=s0_ind[nn-2]:       #alareunan liikkeet ei mahdollisía
+        ignoorauslista[4] = True    #reuna 1 ruudun päässä -> poista ala
+        ignoorauslista[5] = True
+    elif heppa>s0_ind[nn-1]:
+        ignoorauslista[4] = True    #heppa reunalla -> posta ala + sivu alat
+        ignoorauslista[5] = True
+        ignoorauslista[3] = True    
+        ignoorauslista[7] = True
+    print(s0_ind[1],s0_ind[2],s0_ind[nn-2],s0_ind[nn-1])
+
+    # määrittele sopimattomat paikat kun heppa paikassa heppa eli heppa ja mahdolliset ruudut x0-24
+    sopimattomat = heppahyokkaus(heppa,nn)
+    print(sopimattomat)
+    #määrittele sopimattomat paikat kun heppa paikassa heppa ja sijoitetaan kuningatarta
+    
     return laskin
 
 
 def count(nn):
+
+    #handlataan liian pienet kentät joissa ei ollenkaan paikkoja
+    if nn<=3:
+        return 0
 
     paikat=array('I',[1]*nn*nn)     # nn*nn matriisi pötkönä
 
@@ -73,11 +68,11 @@ def count(nn):
         ekat_ind.append(ii*nn)
     print(ekat_ind)
     
-    #testataan eri paikat ratsulle
+    
     laskin=0
     for testipaikka in range(len(paikat)):
         #print(testipaikka)
-        laskin=laskin+testaa(testipaikka,paikat,ekat_ind,nn)  # testataan eri paikat kuningattarelle
+        laskin=laskin+testaa(testipaikka,paikat,ekat_ind,nn)  
         #laskin+=1
     #print(laskin)
 
@@ -85,7 +80,7 @@ def count(nn):
     
 
 if __name__ == "__main__":
-    print(heppahyokkaus(4,3,3)) # 0
-    #print(count(4)) # 40
+    #print(count(3)) # 0
+    print(count(4)) # 40
     #print(count(5)) # 184
     
