0028c60eefaf783a2705553cf63f504f1a82568d
Sanna <santras@users.noreply.github.com>
2020-09-03 20:28:08 +0300

    tsekkaus vas-oik reunat

--- queenknight.py
+++ queenknight.py
@@ -9,12 +9,12 @@ def heppahyokkaus(heppa,nn):
     #Tämä antaa listana indeksit hepan mahdollisille hyökkäysruuduille, kun nn on ruudukon sivun pituus.
     #yla1,yla2,vas1,vas2, ala1,ala2,oik1,oik2
 
-    yla1 = (heppa-2*nn-1) # 0
-    yla2 = (heppa-2*nn+1) #1
+    yla1 = (heppa-2*nn-1) # 0            =yläkerran vas
+    yla2 = (heppa-2*nn+1) #1             = yläkerran oik
     vas1 = (heppa-nn-2)   #2 =vas ylä
     vas2 = (heppa+nn+-2)  #3 =vas ala
-    ala1 = (heppa+2*nn-1) #4
-    ala2 = (heppa+2*nn+1) #5
+    ala1 = (heppa+2*nn-1) #4             = alakerran vas
+    ala2 = (heppa+2*nn+1) #5             = alakerran oik   
     oik1 = (heppa-nn+2)   #6 =oik ylä
     oik2 = (heppa+nn+2)   #7  =oik ala
     
@@ -44,11 +44,34 @@ def testaa(heppa,poyta,s0_ind,nn):
         ignoorauslista[5] = True
         ignoorauslista[3] = True    
         ignoorauslista[7] = True
-    print(s0_ind[1],s0_ind[2],s0_ind[nn-2],s0_ind[nn-1])
+    #print(s0_ind[1],s0_ind[2],s0_ind[nn-2],s0_ind[nn-1])
+    s_pera=s0_ind                           # tokavika sarake
+    for ii in range(len(s_pera)):
+        s_pera[ii] = s_pera[ii]+nn-1  
+    print(s_pera)
+
+   if heppa in s0_ind:              # heppa vas reunalla -> poista vas + yla ja ala 
+        ignoorauslista[2] = True
+        ignoorauslista[3] = True
+        ignoorauslista[0] = True    
+        ignoorauslista[4] = True
+    elif heppa-1 in s0_ind:         #heppa vas yhden ruudun päässä -> poista vas
+        ignoorauslista[2] = True    
+        ignoorauslista[3] = True
+    elif heppa in s_pera:           
+        ignoorauslista[6] = True    # heppa oik reunalla -> poista oik + yla ja ala
+        ignoorauslista[7] = True
+        ignoorauslista[1] = True    
+        ignoorauslista[5] = True
+    elif heppa+1 in s_pera:
+        ignoorauslista[6] = True    # heppa oik reunalla yhden ruudun päässä -> poista oik
+        ignoorauslista[7] = True
+
+
 
     # määrittele sopimattomat paikat kun heppa paikassa heppa eli heppa ja mahdolliset ruudut x0-24
     sopimattomat = heppahyokkaus(heppa,nn)
-    print(sopimattomat)
+    #print(sopimattomat)
     #määrittele sopimattomat paikat kun heppa paikassa heppa ja sijoitetaan kuningatarta
     
     return laskin
