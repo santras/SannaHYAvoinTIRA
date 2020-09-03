f9ffa5fa3123d6646b4c4de2917e539d9faf1216
Sanna <santras@users.noreply.github.com>
2020-09-04 01:20:56 +0300

    tehtavananto

---
+++ rectangles.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+
+
+def area(rec1, rec2, rec3):
+    # TODO
+
+if __name__ == "__main__":
+    rec1 = (-1,1,1,-1)
+    rec2 = (0,3,2,0)
+    rec3 = (0,2,3,-2)
+    print(area(rec1,rec2,rec3)) # 16
