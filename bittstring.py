8298648127dd48386748516967ce737980dac530
Sanna <santras@users.noreply.github.com>
2020-09-14 22:42:05 +0300

    Ehkä nyt

--- bittstring.py
+++ bittstring.py
@@ -7,22 +7,30 @@
 def calculate(ss):
 
     #  Jotta olisi vuoroin 0 ja 1 voi olla joko 0101... tai 1010...
-    parittomat = 0  # parittomat 1:ä
-    parilliset = 0  # parilliset 1:ä
+    alkaa_nollasta = 0  # parilliset indeksit 0:aa [0,2,4...], parittomat 1:stä [1,3,5 ...]
+    alkaa_ykkosesta = 0  # parittomat indeksit 0:aa [1,3,5...], parilliset 1:stä [0,2,4...]
                          
     for ii in range(len(ss)):
-        if ii%2 != 0:   # jonon pariton jäsen
-            if ss[ii] != 1:
-                parittomat += 1
-            else:
-                parilliset += 1
-        else:           # jonon parillinen jäsen
-            if ss[ii] != 1:
-                parilliset += 1
-            else:
-                parittomat += 1
+        #print('ii,luku',ii, ss[ii],ii%2)
+        if ii%2 == 0:   # parilliset indeksit
+            #print('parillinen indeksi')
+            if ss[ii] == '0':         
+                alkaa_ykkosesta += 1
+                #print('alkaa_ykkosesta muuttuu')
+            else:                   # parillinen =0
+                alkaa_nollasta += 1
+                #print('alkaa_nollasta muuttuu')
+        else:           # parittomat indeksit
+            #print('pariton indeksi')
+            if ss[ii] == '0':         # pariton = 1
+                alkaa_nollasta += 1
+                #print('alkaa_nollasta muuttuu')
+            else:                   # pariton =0
+                alkaa_ykkosesta += 1
+                #print('alkaa_ykkosesta muuttuu')
+        #print('----------------')
 
-    return (min(parilliset,parittomat))
+    return (min(alkaa_nollasta,alkaa_ykkosesta))
     
 
 
