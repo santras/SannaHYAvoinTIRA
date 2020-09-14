3f8476bfe57445d0cf338f22607f2b98e7b80379
Sanna <santras@users.noreply.github.com>
2020-09-14 21:43:09 +0300

    No niin nyt lähti pyörimään

--- oncechar.py
+++ oncechar.py
@@ -4,59 +4,23 @@ import sys
 from time import time
 
 
-def slowcount(ss):
-
-    nn = len(ss)
-    apu =
-
-    for ii in range(nn):
-        print('ii',ii)
-        if ii != nn-1:
-            if ss[ii] != ss [ii+1]:
-                print(ii)
-                continue
-        for jj in range ():
-            #print('jj',jj)
-
-            #if ii != jj and ss[jj] !=ss[jj+1]:
-            #    print('ei sama')
-            #print(ss[ii:jj+1])
-            #if ss[ii] != ss[jj]: #and ss[jj] == ss[jj-1]:
-            #    print(ss[ii:jj])
-            #else:
-            #    print(ss[ii:jj])
-
-
-
 def count(ss):
-    setti = list(set (ss))
-    pituudet = {}
-    for ii in range (len(setti)):
-        pituudet[setti[ii]] = 0
 
     #print(pituudet)
     laskin = 1
-    summaus = 0
-    for ii in range(len(ss)):
-        if ii == (len(ss)-1):
-            #print('vika kierros')
-            if pituudet[ss[ii]]<= laskin:
-                    pituudet[ss[ii]] =laskin
+    summa = 1                         # Ensimmäinen kirjain yksittäin laskettu mukaan
+    for ii in range(1,len(ss)):
+        if ss[ii] == ss[ii-1]:           # Onko edellinen sama
+            laskin += 1                  
+            summa += laskin
         else:
-            if ss[ii] == ss[ii+1]:
-                laskin +=1
-                summaus = summaus + laskin 
-            else:
-                #print(pituudet[ss[ii]])
-                if pituudet[ss[ii]]<= laskin:
-                    pituudet[ss[ii]] =laskin
-                laskin = 1
-        
-    print(pituudet)
+            summa += 1
+            laskin =1
+    return summa
     
 
 
 if __name__ == "__main__":
-    #print(count("aaa")) # 6
-    print(slowcount("abbbcaa")) # 11
-    #print(count("abcde")) # 5
+    print(count("aaa")) # 6
+    print(count("abbbcaa")) # 11
+    print(count("abcde")) # 5
