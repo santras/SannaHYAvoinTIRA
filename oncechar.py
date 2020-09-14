3645221a01e2f6de242921ae9c8ecbe7c51a7088
Sanna <santras@users.noreply.github.com>
2020-09-14 17:25:33 +0300

    yrittää

--- oncechar.py
+++ oncechar.py
@@ -4,10 +4,31 @@ import sys
 from time import time
 
 def count(ss):
+    setti = list(set (ss))
+    pituudet = {}
+    for ii in range (len(setti)):
+        pituudet[setti[ii]] = 0
+
+    print(pituudet)
+    laskin = 1
+    for ii in range(len(ss)):
+        if ii == (len(ss)-1):
+            #print('vika')
+        else:
+
+            if ss[ii] == ss[ii+1]:
+                laskin +=1
+                #print('kierros')
+            else:
+                print(pituudet[ss[ii]])
+                #if pituudet[ii]<= laskin
+                laskin = 1
+        
+
     
 
 
 if __name__ == "__main__":
     print(count("aaa")) # 6
-    print(count("abbbcaa")) # 11
-    print(count("abcde")) # 5
+    #print(count("abbbcaa")) # 11
+    #print(count("abcde")) # 5
