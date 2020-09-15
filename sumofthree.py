3f211e123ae0b8cef59175395a9820120616994a
Sanna <santras@users.noreply.github.com>
2020-09-15 15:24:57 +0300

    Ei toimi näin --> uusi yritys

--- sumofthree.py
+++ sumofthree.py
@@ -6,10 +6,23 @@
 
 
 
-def count(n):
-    # TODO
+def count(nn):
+    luku_pikkuset = 1
+    laskin = 1
+
+    for ii in range (nn):
+        kolmas = nn - (luku_pikkuset * 2 + 1)
+        print (luku_pikkuset, luku_pikkuset+1, kolmas)
+        if kolmas <= (luku_pikkuset +1):
+            return laskin
+        else:
+            laskin += 1
+            luku_pikkuset += 1 
+
+
+
 
 if __name__ == "__main__":
     print(count(8)) # 2
-    print(count(30)) # 61
-    print(count(1337)) # 148296
+    #print(count(30)) # 61
+    #print(count(1337)) # 148296
