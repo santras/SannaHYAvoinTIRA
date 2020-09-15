0a08b2b231606bfdd4f994e40f06de720c104790
Sanna <santras@users.noreply.github.com>
2020-09-15 17:15:18 +0300

    Ei toimi tämäkään

--- sumofthree.py
+++ sumofthree.py
@@ -3,21 +3,29 @@
 # -*- coding: utf-8 -*- 
 #import sys
 #from time import time
+import math
 
 
 
-def count_ei(nn):                       # isoin on max puolet
-    luku_pikkuset = 1
-    laskin = 1
+def count_uus(nn):                       # isoin min 12 max 27 (n-3) # pienin 1-8, keskim 2-14
 
-    for ii in range (nn):
-        kolmas = nn - (luku_pikkuset * 2 + 1)
-        print (luku_pikkuset, luku_pikkuset+1, kolmas)
-        if kolmas <= (luku_pikkuset +1):
-            return laskin
-        else:
-            laskin += 1
-            luku_pikkuset += 1 
+    kerroslaskin = 1
+    laskin = 0
+
+    # 27 1,2
+    # 26 1,3 (2,2)
+    # 25 1,4  2,3
+    # 24 1,5  2,4  (3,3)
+
+    for ii in range (nn-3,math.floor(nn/3),-1):                     
+        #print (ii)                                                  
+        laskin += kerroslaskin                  # Lisätään joka kerroksella kerroslaskimen verran vaihtoehtoja laskimeen                                                    
+        if ii % 2 ==0:                          # Parillisen luvun jälkeen kerroslaskuriin tulee yksi lisää                                         
+            kerroslaskin += 1
+        
+
+
+    return laskin      
 
 def count(nn):
 
@@ -27,19 +35,20 @@ def count(nn):
     for ii in range (1,nn):
         for jj in range (1,nn):
             kolmas = nn -ii -jj
-            #print (ii,jj, kolmas)
-            if ii != jj and kolmas != ii and kolmas != jj and kolmas >0:                    # luvut eri järjestyksessä pitäis passata mutta jotain vikaa seuraavassa
-                print (sorted([ii,jj, kolmas]))
+            
+            if ii != jj and kolmas != ii and kolmas != jj and kolmas >0:                   
+                #print (sorted([ii,jj, kolmas]))
+                #print ((sorted([ii,jj, kolmas])[1]))
                 if sorted([ii,jj, kolmas]) not in kaytetyt :
                     kaytetyt.append(sorted([ii,jj,kolmas]))
                     laskin += 1
-                    #print(ii,jj,kolmas)
+                   
 
-    
+    #print (kaytetyt)
     return laskin
 
 
 if __name__ == "__main__":
     #print(count(8)) # 2
-    print(count(30)) # 61
+    print(count_uus(30)) # 61
     #print(count(1337)) # 148296
