6b519a7553e8c98f604cbd4ff931b4aa6688c261
Sanna <santras@users.noreply.github.com>
2020-09-15 11:59:47 +0300

    indeksit sotkussa

--- splitlist.py
+++ splitlist.py
@@ -16,22 +16,22 @@ def count(lista):
         if pikkuset == []:
             pikkuset.append(lista[ii])
         else:
-            pikkuset.append(min(pikkuset[ii-1],lista[ii]))
+            pikkuset.append(max(pikkuset[ii-1],lista[ii]))
 
     for ii in range (ll-1,-1,-1):
         #print(ii)
         if ii == ll-1:
             isot[ii] = lista [ii]
         else:
-            isot [ii] = max(isot[ii+1],lista[ii])
+            isot [ii] = min(isot[ii+1],lista[ii])
 
-    #print(len(lista))
-    #print(len(pikkuset))
-    #print(len(isot))
+    print(lista)
+    print(pikkuset)
+    print(isot)
     laskuri = 0
     
-    for ii in range (ll-1):
-        if pikkuset[ii]<isot[ii]:
+    for ii in range (1,ll-1):
+        if pikkuset[ii-1]<isot[ii]:
             laskuri += 1
             #print(ii)
 
@@ -40,6 +40,6 @@ def count(lista):
 
 
 if __name__ == "__main__":
-    print(count([1,2,3,4,5])) # 4
+    #print(count([1,2,3,4,5])) # 4
     #print(count([5,4,3,2,1])) # 0
-    #print(count([2,1,2,5,7,6,9])) # 3
+    print(count([2,1,2,5,7,6,9])) # 3
