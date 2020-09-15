8364b7c7d83cc1974796ac73474b5d950ba3a876
Sanna <santras@users.noreply.github.com>
2020-09-15 20:18:15 +0300

    muutinko jo jotain?

--- sumofthree.py
+++ sumofthree.py
@@ -32,31 +32,39 @@ def count (nn):
     testilaskin =0
     kerroslaskin = 1
 
-    for ii in range (nn-3,math.floor(nn/3),-1):    
+    apu math.floor(nn/4)
+
+    if nn%2 == 0                                                    # Alustetaan laskin puoleen väliin
+        laskin = apu* (apu+1)
+    else:
+        laskin = math.floor((apu*(apu+1))/2) + math.floor(((apu-1)*(apu))/2)
+
+
+    for ii in range (math.floor(nn/2),math.floor(nn/3),-1):    
         #print('iso',ii)
         toinen_max = (nn-ii-1)
         testilaskin += laskin2
         laskin2 = 0
         
-        if ii >= math.floor(nn/2):                  # Ensimmäisistä kerroksista löytyi kaava puoleen väliin alkiota
-            laskin += kerroslaskin
-            if ii % 2 ==0:                          # Parillisen luvun jälkeen kerroslaskuriin tulee yksi lisää                                         
-                kerroslaskin += 1
+        #if ii >= math.floor(nn/2):                  # Ensimmäisistä kerroksista löytyi kaava puoleen väliin alkiota
+            #laskin += kerroslaskin
+            #if ii % 2 ==0:                          # Parillisen luvun jälkeen kerroslaskuriin tulee yksi lisää                                         
+            #    kerroslaskin += 1
         
-        else:
-            for jj in range(toinen_max,1,-1):
-                #print('toka',jj)
-                eka=nn-jj-ii
-                #print('eka',eka)
-                #print(eka,jj,ii)
-                if eka >= jj:
-                    print('Kerros',ii,jj,laskin2)
-                    break
-                if jj >= ii:
-                    continue
-                #print(eka,jj,ii)
-                laskin += 1 
-                laskin2 += 1
+        #else:
+        for jj in range(toinen_max,1,-1):
+            #print('toka',jj)
+            eka=nn-jj-ii
+            #print('eka',eka)
+            #print(eka,jj,ii)
+            if eka >= jj:
+                print('Kerros',ii,jj,laskin2)
+                break
+            if jj >= ii:
+                continue
+            #print(eka,jj,ii)
+            laskin += 1 
+            laskin2 += 1
 
     #print('testilaskin',testilaskin)
     return laskin
@@ -64,7 +72,8 @@ def count (nn):
 
 if __name__ == "__main__":
     #print(count(8)) # 2
-    print(count(30)) # 61
+    #print(count(30)) # 61
+    print(count(32))
     #print(count(50))
     #print(count(1337)) # 148296
     #print (count(663307073)) # iso testi feilas
