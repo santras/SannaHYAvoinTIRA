0705170f629484b744cd9b261ad5c40d557bda39
Sanna <santras@users.noreply.github.com>
2020-09-15 12:05:53 +0300

    alku

---
+++ twochar.py
@@ -0,0 +1,15 @@
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+#import sys
+#from time import time
+
+
+
+def count(s):
+    # TODO
+
+if __name__ == "__main__":
+    print(count("aaaa")) # 0
+    print(count("abab")) # 6
+    print(count("aabacba")) # 8
