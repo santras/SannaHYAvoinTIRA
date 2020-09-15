7ab80ad06c3eca27bf3e16d189af1c3f616bef45
Sanna <santras@users.noreply.github.com>
2020-09-15 15:11:32 +0300

    aloitus

---
+++ sumofthree.py
@@ -0,0 +1,15 @@
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+#import sys
+#from time import time
+
+
+
+def count(n):
+    # TODO
+
+if __name__ == "__main__":
+    print(count(8)) # 2
+    print(count(30)) # 61
+    print(count(1337)) # 148296
